f9280d52bcb2418c105917e2c138da237958cecc
dinhquoc1st <49915527+dinhquoc1st@users.noreply.github.com>
2026-09-22 18:38:48 +0700

    Update backend URL to new chat-stranger instance

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 // Backend URL. Change this to point at a different chat-stranger instance.
 // export const HOST = 'http://localhost:5000';
-export const HOST = 'https://chat-stranger.onrender.com';
+export const HOST = 'https://tramla.dinhquoc.io.vn';
